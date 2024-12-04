5e8589c112d5a9f3d493e4681e9241c028cf47e2
daridev <darideveloper@gmail.com>
2024-12-04 16:05:58 -0600

    fix (next-intl): use 'es' as default locale

--- middleware.js
+++ middleware.js
@@ -2,11 +2,13 @@ import { NextResponse } from 'next/server'
 import createMiddleware from 'next-intl/middleware'
 import { routing } from '@/i18n/routing'
 
-export async function middleware(request) {
+export default async function middleware(request) {
+
+  const url = new URL(request.url)
+  const pathname = url.pathname
+
   // Check if the request is for an API route
-  if (request.url.includes('/api')) {
-    // Skip internationalization logic for API routes
-    console.debug('No token found, logging in...')
+  if (pathname.includes('/api')) {
 
     const username = process.env.DASHBOARD_USER
     const password = process.env.DASHBOARD_PASSWORD
@@ -45,7 +47,29 @@ export async function middleware(request) {
         { status: 500, headers: { 'Content-Type': 'application/json' } }
       )
     }
-    
+
+  }
+
+  // Check if the request already has a locale in the URL
+  const hasLocale = /^\/(es|en)(\/|$)/.test(pathname)
+
+  // Check for the `lang` cookie
+  const langCookie = request.cookies.get('NEXT_LOCALE')?.value
+
+  console.log ({ hasLocale, langCookie })
+
+  // If there's no locale in the URL and no `lang` cookie, redirect to the default locale
+  if (!hasLocale && !langCookie) {
+    console.debug('Locale missing and no lang cookie found, redirecting to default...')
+    url.pathname = `/${routing.defaultLocale}${pathname}`
+    return NextResponse.redirect(url)
+  }
+
+  // If the `lang` cookie exists but no locale is in the URL, redirect to the cookie's locale
+  if (!hasLocale && langCookie && routing.locales.includes(langCookie)) {
+    console.debug(`Locale missing, but lang cookie found: ${langCookie}. Redirecting...`)
+    url.pathname = `/${langCookie}${pathname}`
+    return NextResponse.redirect(url)
   }
 
   // Add internationalization middleware for other routes
